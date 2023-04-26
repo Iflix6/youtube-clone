7101728ef8ed3e75ba4d96a7815cee85eefbf3bd
INAKHE ISRAEL <israelinakhe@gmail.com>
2023-04-26 13:18:17 +0100

    Done

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { fetchFromAPI } from "../utils/fetchFromAPI";
-import { Videos, SideBar } from "./";
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
@@ -18,11 +18,11 @@ const Feed = () => {
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
-        <SideBar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
+        <Sidebar selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory} />
         
-      
-        </Typography> variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
-          Copyright © 2023 INAKHE ISRAEL
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
+        </Typography>
       </Box>
 
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
@@ -33,7 +33,6 @@ const Feed = () => {
         <Videos videos={videos} />
       </Box>
     </Stack>
-    
   );
 };
 
